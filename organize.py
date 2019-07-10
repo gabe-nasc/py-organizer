54ff81c7fe8fb443bd82ede9beaeaddcd2f4bb8d
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-07-10 17:19:10 -0300

    Add categorize function Add a print that confirms to the user which folder is getting organized Fixed path problems with find_files function

--- organize.py
+++ organize.py
@@ -1,10 +1,42 @@
 import os, sys
 
+categories = {
+    "Images":["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff", "tif", "psd", "raw", "arw", "cr2", "nrw", "k25", "bmp", "svg", "svgz"],
+    "Audio":["mp3", "flac", "ogg", "wav", "aac", "aa", "aax", "3gp", "m4a", "m4b", "m4p", "oga"],
+    "Video":["webm", "mp4", "avi", "mkv", "flv", "ogv", "gifv", "mov", "qt", "mpg", "mp2", "mpeg", "mpv", "3gp"],
+    "Code":["py", "c", "cpp", "js", "html", "css", "java", "go", "php"],
+    "Docs":["doc", "dot", "docx", "docm", "xls", "xlt", "xlsx", "xlsm", "xltx", "xltm", "ppt", "pptx", "pptm", "pps", "potx", "potm", "ppsx", "pdf", "epub", "mobi"],
+    "Executables":["exe", "run"],
+}
+
 def find_files(path):
-    return [f for f in os.listdir() if os.path.isfile(f)]
+    return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 def get_extensions(files):
-    return [f.lower().split('.')[-1] for f in files]
+    return [(f,f.lower().split('.')[-1]) for f in files]
+
+def categorize(files):
+    file_ext = get_extensions(files)
+
+    categorized_files = {
+        "Executables":[],
+        "Images":[],
+        "Audio":[],
+        "Video":[],
+        "Code":[],
+        "Docs":[],
+        "Unknown":[],
+    }
+
+    for file, ext in file_ext:
+        for cat in categories:
+            if ext in categories[cat]:
+                categorized_files[cat].append(file)
+                break
+        else:
+            categorized_files["Unknown"].append(file)
+    
+    return categorized_files
 
 def main():
     if len(sys.argv) < 2:
@@ -15,6 +47,11 @@ def main():
     if not os.path.exists(path):
         print("ERROR: Path does not exist")
         exit()
+
+    print("Files for: ", path)
+
+    file_names = find_files(path)
+    cf = categorize(file_names)
 
 if __name__ == "__main__":
     main()

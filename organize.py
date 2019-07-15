ab07ea6cf1383a77bf9f4f30082b6da3cdf63929
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-07-15 15:41:01 -0300

    Add the feature to move to the OS default user folders Add functions to detect the OS default folders Add print statements to show the users which files are being moved where Add argument checking to the input part so the user can define which mode it wants Add virtual enviroment and vscode folders to gitignore

--- organize.py
+++ organize.py
@@ -1,31 +1,58 @@
-import os, sys
+import platform
+import sys
+import os
+
 
 categories = {
-    "Images":["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff", "tif", "psd", "raw", "arw", "cr2", "nrw", "k25", "bmp", "svg", "svgz"],
-    "Audio":["mp3", "flac", "ogg", "wav", "aac", "aa", "aax", "3gp", "m4a", "m4b", "m4p", "oga"],
-    "Video":["webm", "mp4", "avi", "mkv", "flv", "ogv", "gifv", "mov", "qt", "mpg", "mp2", "mpeg", "mpv", "3gp"],
-    "Code":["py", "c", "cpp", "js", "html", "css", "java", "go", "php"],
-    "Docs":["doc", "dot", "docx", "docm", "xls", "xlt", "xlsx", "xlsm", "xltx", "xltm", "ppt", "pptx", "pptm", "pps", "potx", "potm", "ppsx", "pdf", "epub", "mobi"],
-    "Executables":["exe", "run"],
+    "Images": ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff", "tif", "psd", "raw", "arw", "cr2", "nrw", "k25", "bmp", "svg", "svgz"],
+    "Audio": ["mp3", "flac", "ogg", "wav", "aac", "aa", "aax", "3gp", "m4a", "m4b", "m4p", "oga"],
+    "Video": ["webm", "mp4", "avi", "mkv", "flv", "ogv", "gifv", "mov", "qt", "mpg", "mp2", "mpeg", "mpv", "3gp"],
+    "Code": ["py", "c", "cpp", "js", "html", "css", "java", "go", "php"],
+    "Docs": ["doc", "dot", "docx", "docm", "xls", "xlt", "xlsx", "xlsm", "xltx", "xltm", "ppt", "pptx", "pptm", "pps", "potx", "potm", "ppsx", "pdf", "epub", "mobi"],
+    "Executables": ["exe", "run"],
 }
+
+def windows_user_paths():
+    from win32com.shell import shell, shellcon  # BAD PRACTICE, haven't found a better way getting around this, sue me
+
+    paths = {
+        "Images": shell.SHGetFolderPath(0, shellcon.CSIDL_MYPICTURES, None, 0),
+        "Audio": shell.SHGetFolderPath(0, shellcon.CSIDL_MYMUSIC, None, 0),
+        "Video": shell.SHGetFolderPath(0, shellcon.CSIDL_MYVIDEOS, None, 0),
+        "Docs": shell.SHGetFolderPath(0, shellcon.CSIDL_MYDOCUMENTS, None, 0),
+    }
+
+    return paths
+
+def linux_user_paths():
+    from gi.repository import GLib
+
+    paths = {
+        "Images": GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES),
+        "Audio": GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_MUSIC),
+        "Video": GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_VIDEOS),
+        "Docs": GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOCUMENTS),
+    }
+
+    return paths
 
 def find_files(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 def get_extensions(files):
-    return [(f,f.lower().split('.')[-1]) for f in files]
+    return [(f, f.lower().split('.')[-1]) for f in files]
 
 def categorize(files):
     file_ext = get_extensions(files)
 
     categorized_files = {
-        "Executables":[],
-        "Images":[],
-        "Audio":[],
-        "Video":[],
-        "Code":[],
-        "Docs":[],
-        "Unknown":[],
+        "Executables": [],
+        "Images": [],
+        "Audio": [],
+        "Video": [],
+        "Code": [],
+        "Docs": [],
+        "Unknown": [],
     }
 
     for file, ext in file_ext:
@@ -35,37 +62,71 @@ def categorize(files):
                 break
         else:
             categorized_files["Unknown"].append(file)
-    
-    return {key:value for key, value in categorized_files.items() if len(value) > 0}
+
+    return {key: value for key, value in categorized_files.items() if len(value) > 0}
+
 
 def create_folders(path, keys):
     for folder in keys:
         new_path = os.path.join(path, folder)
-        os.mkdir(new_path)
+        try:
+            os.mkdir(new_path)
+        except FileExistsError:
+            pass
 
-def move_files(path, cfiles):
-    for folder, files in cfiles.items():
-        for f in files:
-            os.rename(os.path.join(path, f), os.path.join(path, folder, f))
 
+def move_files(path, cfiles, user):
+    if not user:
+        create_folders(path, cfiles.keys())
+        for folder, files in cfiles.items():
+            for f in files:
+                start = os.path.join(path, f)
+                destination = os.path.join(path, folder, f)
+
+                os.rename(start, destination)
+
+                print("\nFile:", start, "\nMoved to:", destination)
+
+    else:
+        if platform.system() == "Linux":
+            user_paths = linux_user_paths()
+        else:
+            user_paths = windows_user_paths()
+
+        create_folders(path, [folder for folder in cfiles if folder not in user_paths])
+
+        for folder, files in cfiles.items():
+            for f in files:
+                start = os.path.join(path, f)
+
+                if folder in user_paths:
+                    destination = os.path.join(user_paths[folder], f)
+                else:
+                    destination = os.path.join(path, folder, f)
+
+                os.rename(start, destination)
+
+                print("\nFile:", start, "\nMoved to:", destination)
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv[1:]) and not os.path.exists(sys.argv[-1]):
         path = os.getcwd()
+        print("No valid path found as argument, organizing:", )
     else:
-        path = sys.argv[1]
-    
-    if not os.path.exists(path):
-        print("ERROR: Path does not exist")
-        exit()
+        path = sys.argv[-1]
+
+    if "-u" in sys.argv:
+        user = True
+    else:
+        user = False
 
     print("Files for:", path)
 
     file_names = find_files(path)
     cf = categorize(file_names)
-    create_folders(path, cf.keys())
-    move_files(path, cf)
-    print(len(file_names), "files were moved.")
+
+    move_files(path, cf, user)
+    print("\n", len(file_names), "files were moved.")
 
 if __name__ == "__main__":
     main()
